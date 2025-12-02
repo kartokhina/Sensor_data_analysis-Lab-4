350552bb5163b8f9e7fe5bd1a1bdf429557b5e23
kartokhina <fine.victor1ja@yandex.ru>
2025-12-03 00:46:33 +0300

    Через арифметику указателей

--- Sensor_data_analysis.cpp
+++ Sensor_data_analysis.cpp
@@ -6,7 +6,7 @@ void inputData(float** data, int d, int h){
     for (int i = 0; i < d; i++){
         cout << "День " << i + 1 << ": ";
         for (int j = 0; j < h; j++) {
-            cin >> data[i][j];
+            cin >> *(*(data + i)+j);
         }
         
     }
@@ -16,7 +16,7 @@ void printData(float** data, int d, int h) {
     for (int i = 0; i < d; i++) {
         cout << "День " << i + 1 << ": ";
         for (int j = 0; j < h; j++) {
-            cout << data[i][j] << " ";
+            cout <<  *(*(data + i)+j) << " ";
         }
         cout << endl;
     }
@@ -31,9 +31,9 @@ float dayAverage(const float* row, int h){
 
 float overallAverage(float** data, int d, int h) {
     float t = 0.0;
-    for (int i; i < d; i++){
-        for (int j; j < h; j++){
-            t += data[i][j];
+    for (int i=0; i < d; i++){
+        for (int j=0; j < h; j++){
+            t +=  *(*(data + i)+j);
         }
     }
     return t / (d * h);
@@ -42,7 +42,7 @@ float overallAverage(float** data, int d, int h) {
 void showHotDays(float** p, int d, int h){
     float overallAvg = overallAverage(p, d, h);
     for (int i; i < d; i++){
-        float dayAvg = dayAverage(p[i], h);
+        float dayAvg = dayAverage(*(p + i), h);
         if (dayAvg > overallAvg){
              cout << "День " << i + 1 << " (среднее = " << dayAvg << ")" << endl;
         }
@@ -62,7 +62,7 @@ int main() {
     
     float** data = new float* [d];
     for (int i = 0; i < d; i++){
-        data[i] = new float[h];
+        *(data + i) = new float[h];
     }
     
 
@@ -78,7 +78,7 @@ int main() {
 
 
     for (int i = 0; i < d; i++) {
-        delete[] data[i];
+        delete[] *(data+i);
     }
     delete[] data;
 
